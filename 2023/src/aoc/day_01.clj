52c827bd758afc2fe2bf40486a5f59898d6f3094
Michael Whalen <michael@whalesalad.com>
2023-12-06 22:07:23 -0500

    day 1, part 2 is much closer, but still fucked

--- 2023/src/aoc/day_01.clj
+++ 2023/src/aoc/day_01.clj
@@ -1,5 +1,6 @@
 (ns aoc.day-01
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string]
+            [clojure.pprint :as pp]))
 
 (def replacements
   {"zero" "0"
@@ -14,29 +15,36 @@
    "nine" "9"})
 
 (def part-1-re #"\d")
-(def part-2-re #"\d|one|two|three|four|five|six|seven|eight|nine|ten")
+(def part-2-re #"(?=(\d|one|two|three|four|five|six|seven|eight|nine|ten))")
+
+(defn get-digit
+  "Find the first digit in a string of text"
+  [text replace-words? getter]
+  (let [re (if replace-words? part-2-re part-1-re)
+        matches (map last (re-seq re text))
+        match (str (getter matches))]
+    (if replace-words?
+      (get replacements match match)
+      match)))
 
 (defn first-digit
   "Find the first digit in a string of text"
-  [text]
-  (let [digit (first (re-seq part-2-re text))]
-    (if-let [replacement (get replacements digit)]
-      replacement
-      digit)))
+  ([text] (first-digit text false))
+  ([text replace-words?] (get-digit text replace-words? first)))
 
 (defn last-digit
   "Find the last digit in a string of text"
-  [text]
-  (first (reverse (re-seq part-2-re text))))
+  ([text] (last-digit text false))
+  ([text replace-words?] (get-digit text replace-words? last)))
 
 (defn integer-from-digits
   [first last]
   (Integer/parseInt (str first last)))
 
 (defn line-to-int
-  [line]
-  (let [first (first-digit line)
-        last (last-digit line)]
+  [line replace-words?]
+  (let [first (first-digit line replace-words?)
+        last (last-digit line replace-words?)]
     (integer-from-digits first last)))
 
 (defn part-1
@@ -44,11 +52,17 @@
   [input]
   (println "input:" input)
   (let [lines (string/split-lines input)
-        integers (map line-to-int lines)]
+        to-int (fn [line] (line-to-int line false))
+        integers (map to-int lines)]
     (println "integers:" integers)
     (reduce + integers)))
 
 (defn part-2
   "Day 01 Part 2"
   [input]
-  nil)
+  (println "input:" input)
+  (let [lines (string/split-lines input)
+        to-int (fn [line] (line-to-int line true))
+        integers (map to-int lines)]
+    (pp/pprint (zipmap lines integers))
+    (reduce + integers)))
