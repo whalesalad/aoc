b95df432274145d33c79dddcf2c40cc68812291f
Michael Whalen <michael@whalesalad.com>
2023-12-07 11:21:08 -0500

    add day 3

---
+++ 2023/src/aoc/day_03.clj
@@ -0,0 +1,25 @@
+(ns aoc.day-02
+  (:require [clojure.string :as str]
+            [clojure.pprint :as pp]))
+
+(defn schematic
+"467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..")
+
+(defn part-1
+  "Day 03 Part 1"
+  [input]
+  nil)
+
+(defn part-2
+  "Day 03 Part 2"
+  [input]
+  nil)
