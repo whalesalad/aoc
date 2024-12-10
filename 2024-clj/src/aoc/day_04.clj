4f14e1df1700ba63ec3e75b659714404ce0ce4b0
Michael Whalen <michael@whalesalad.com>
2024-12-09 21:38:09 -0500

    scaffold script, start of day 4

---
+++ 2024-clj/src/aoc/day_04.clj
@@ -0,0 +1,40 @@
+(ns aoc.day-04)
+
+(def test-data
+  "MMMSXXMASM
+MSAMXMSMSA
+AMXSXMAAMM
+MSAMASMSMX
+XMASAMXAMM
+XXAMMXXAMA
+SMSMSASXSS
+SAXAMASAAA
+MAMMMXMMMM
+MXMXAXMASX")
+
+(def test-data-human
+  "....XXMAS.
+.SAMXMS...
+...S..A...
+..A.A.MS.X
+XMASAMX.MM
+X.....XA.A
+S.S.S.S.SS
+.A.A.A.A.A
+..M.M.M.MM
+.X.X.XMASX")
+
+
+(defn part-1
+  "Day 04 Part 1"
+  [input]
+  (println "input:" input)
+  ;; Implement part 1 logic here
+  )
+
+(defn part-2
+  "Day 04 Part 2"
+  [input]
+  (println "input:" input)
+  ;; Implement part 2 logic here
+  )
---
+++ 2024-clj/test/aoc/day_04_test.clj
@@ -0,0 +1,9 @@
+(ns aoc.day-04-test
+  (:require [clojure.test :refer [deftest is]]
+            [aoc.day-04 :refer [part-1 part-2]]))
+
+(deftest test-part-1
+  (is (= nil (part-1 nil)))) ; Replace with actual test
+
+(deftest test-part-2
+  (is (= nil (part-2 nil)))) ; Replace with actual test
