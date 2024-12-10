7e614fdf310a1006174b7f2ea59d54803578089b
Michael Whalen <michael@whalesalad.com>
2024-12-09 21:30:04 -0500

    cljeanup

--- 2024-clj/src/aoc/day_03.clj
+++ 2024-clj/src/aoc/day_03.clj
@@ -1,19 +1,11 @@
-(ns aoc.day-03
-  (:require [clojure.string :as str]
-            [clojure.pprint :as pp]))
+(ns aoc.day-03)
 
 (def sample-data
-  "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))")
-
-(def sample-data-part-2
-  "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))")
+  {:part1 "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
+   :part2 "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"})
 
 (def part-1-re #"(mul\((?<a>\d+),(?<b>\d+)\))")
 (def part-2-re #"(mul\((?<a>\d+),(?<b>\d+)\)|((don't)|(do))\(\))")
-
-(defn str-to-int
-  [str]
-  (Integer. str))
 
 (defn extract-pair
   [row]
@@ -30,50 +22,49 @@
   (let [matches (re-seq part-2-re input)]
     matches))
 
-(defn calculate-pairs
+(defn sum-pairs
   [pairs]
   (reduce + (map (fn [[a b]] (* a b)) pairs)))
+
+(defn valid-pairs-part-2
+  [data]
+  (reduce
+   (fn [acc row]
+     (let [{:keys [mode result]} acc
+           first-item (first row)]
+       (cond
+         ;; switch to drop mode on "don't()"
+         (= first-item "don't()") {:mode :drop :result result}
+
+         ;; switch back to capture mode on "do()"
+         (= first-item "do()") {:mode :capture :result result}
+
+         ;; capture pairs in capture mode
+         (and (= mode :capture) (re-find #"mul" first-item))
+         {:mode :capture :result (conj result (extract-pair row))}
+
+         ;; else, continue with current mode
+         :else acc)))
+   ;; initial state
+   {:mode :capture :result []}
+   ;; input data
+   data))
 
 (defn part-1
   "Day 03 Part 1"
   [input]
   (println "input:" input)
   (let [parsed-input (parse-input-day-1 input)
-        output (calculate-pairs parsed-input)]
+        output (sum-pairs parsed-input)]
     (println "output:" output)
     output))
-
-
-(defn process-part-2
-  [data]
-  (let [result (reduce
-                (fn [acc row]
-                  (let [{:keys [mode result]} acc
-                        first-item (first row)]
-                    (cond
-                      ;; switch to drop mode on "don't()"
-                      (= first-item "don't()") {:mode :drop :result result}
-
-                      ;; switch back to capture mode on "do()"
-                      (= first-item "do()") {:mode :capture :result result}
-
-                      ;; capture pairs in capture mode
-                      (and (= mode :capture) (re-find #"mul" first-item))
-                      {:mode :capture :result (conj result (extract-pair row))}
-
-                      ;; else, continue with current mode
-                      :else acc)))
-                ;; initial state
-                {:mode :capture :result []}
-                ;; input data
-                data)]
-    (calculate-pairs (:result result))))
 
 (defn part-2
   "Day 03 Part 2"
   [input]
   (println "input:" input)
   (let [parsed-input (parse-input-day-2 input)
-        output (process-part-2 parsed-input)]
+        valid-pairs (valid-pairs-part-2 parsed-input)
+        output (sum-pairs (:result valid-pairs))]
     (println "output:" output)
     output))
--- 2024-clj/test/aoc/day_03_test.clj
+++ 2024-clj/test/aoc/day_03_test.clj
@@ -1,70 +1,19 @@
 (ns aoc.day-03-test
-  (:require [clojure.test :refer [deftest testing is]]
-            [clojure.string :as string]
+  (:require [clojure.test :refer [deftest is]]
             [aoc.day-03 :refer [sample-data
-                                sample-data-part-2
                                 parse-input-day-1
-                                calculate-pairs
                                 parse-input-day-2
-                                process-part-2]]))
+                                sum-pairs
+                                valid-pairs-part-2]]))
 
 
-(def test-data (parse-input-day-1 sample-data))
+
+(deftest test-sum-pairs
+  (let [data (parse-input-day-1 (:part1 sample-data))]
+    (is (= 161 (sum-pairs data)))))
 
 
-(deftest test-calculate-pairs
-  (is (= 161 (calculate-pairs test-data))))
-
-
-(deftest test-process-part-2
-  (let [data (parse-input-day-2 sample-data-part-2)
-        processed (process-part-2 data)]
-    (is (= 48 processed))))
-
-;; (deftest test-first-digit-replace
-;;   (is (= "2" (first-digit "two1nine" true)))
-;;   (is (= "8" (first-digit "eightwothree" true)))
-;;   (is (= "1" (first-digit "abcone2threexyz" true)))
-;;   (is (= "2" (first-digit "xtwone3four" true)))
-;;   (is (= "4" (first-digit "4nineeightseven2" true)))
-;;   (is (= "1" (first-digit "zoneight234" true)))
-;;   (is (= "7" (first-digit "7pqrstsixteen" true))))
-
-;; (deftest test-last-digit
-;;   (is (= "2" (last-digit "1abc2")))
-;;   (is (= "8" (last-digit "pqr3stu8vwx")))
-;;   (is (= "7" (last-digit "treb7uchet"))))
-
-;; (deftest test-last-digit-replace
-;;   (is (= "9" (last-digit "two1nine" true)))
-;;   (is (= "3" (last-digit "eightwothree" true)))
-;;   (is (= "3" (last-digit "abcone2threexyz" true)))
-;;   (is (= "4" (last-digit "xtwone3four" true)))
-;;   (is (= "2" (last-digit "4nineeightseven2" true)))
-;;   (is (= "4" (last-digit "zoneight234" true)))
-;;   (is (= "6" (last-digit "7pqrstsixteen" true)))
-;;   (is (= "8" (last-digit "abc2x3oneight" true))))
-
-
-;; (deftest test-integer-from-digits
-;;   (let [cases [["1" "2" 12]
-;;                ["2" "5" 25]
-;;                ["9" "9" 99]]]
-;;     (doseq [[first second expected] cases]
-;;       (is (= expected
-;;              (integer-from-digits first second))))))
-
-;; (deftest test-line-to-int-replace
-;;   (doseq [[line expected] test-data-part-2]
-;;     (is (= expected (line-to-int line true)))))
-
-;; (deftest part1
-;;   (let [expected 142
-;;         actual (part-1 test-data)]
-;;     (is (= expected actual))))
-
-;; (deftest part2
-;;   (let [test-data (string/join "\n" (map first test-data-part-2))
-;;         actual (part-2 test-data)
-;;         expected 281]
-;;     (is (= expected actual))))
+(deftest test-part-2
+  (let [data (parse-input-day-2 (:part2 sample-data))
+        processed (:result (valid-pairs-part-2 data))]
+    (is (= 48 (sum-pairs processed)))))
